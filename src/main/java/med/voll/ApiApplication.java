2e3426a108e4d1d03bdba9b1a195b64b24fe9312
DiegoMiranda-dev <mirandadiego864@gmail.com>
2024-07-15 00:54:00 -0600

    executable

--- src/main/java/med/voll/ApiApplication.java
+++ src/main/java/med/voll/ApiApplication.java
@@ -1,4 +1,4 @@
-package med.voll.api;
+package med.voll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
