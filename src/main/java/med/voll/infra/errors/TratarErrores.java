7d90456cc1d87f6792cf7ba2c7319feb8d94824d
DiegoMiranda-dev <mirandadiego864@gmail.com>
2024-07-15 20:10:31 -0600

    dealing with status errors

---
+++ src/main/java/med/voll/infra/errors/TratarErrores.java
@@ -0,0 +1,29 @@
+package med.voll.infra.errors;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratarErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e) {
+        var err = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(err);
+    }
+
+    private record DatosErrorValidacion(String campo, String error){
+        public DatosErrorValidacion(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
